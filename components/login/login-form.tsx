4762fcd879aa751ea103431fb1b179acbcbdada9
Pratyush-Udhani <pratyush.udhani@gmail.com>
2022-12-28 23:42:56 +0530

    test

--- components/login/login-form.tsx
+++ components/login/login-form.tsx
@@ -20,7 +20,7 @@ export default function LoginForm() {
 					values: Values,
 					{ setSubmitting }: FormikHelpers<Values>
 				) => {
-                    console.log("test 3")
+                    console.log("test 4")
 					/* TODO: add empty object check util function*/
 				}}
 			>
